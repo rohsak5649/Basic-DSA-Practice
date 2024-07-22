d5fae2f805c0386ba4503c02ba57bbe7865051a9
Rohan Sakhare <126242412+rohsak5649@users.noreply.github.com>
2024-07-22 14:49:18 +0530

    Create BubbleSort.cpp

---
+++ BubbleSort.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+void bubbleSort(int arr[], int n) {
+    for (int i = 0; i < n - 1; i++) {
+        for (int j = 0; j < n - 1 - i; j++) { // Slight optimization to reduce the number of comparisons
+            if (arr[j] > arr[j + 1]) {
+                int temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+    }
+}
+
+int main() {
+    int arr[] = {12, 23, 2, 32, 1, -12};
+    int n = sizeof(arr) / sizeof(arr[0]);
+
+    bubbleSort(arr, n);
+
+    cout << "Sorted array: ";
+    for (int i = 0; i < n; i++) {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+
+    return 0;
+}
